44ea61b1ccc56c0d327631600f2e6c863425bf89
Marcin Lasak <marcin.lasak@surferseo.com>
2026-04-19 11:04:59 +0200

    chore: Brew setup

---
+++ run_once_before_install-homebrew.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v brew >/dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
